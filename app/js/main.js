73ed0c6c5b8e0f12b0e6e05fbd8300fae9213d33
Bekzod Developer <bkzd18kholdarov@gmail.com>
2022-06-04 11:41:53 +0500

    create project

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+// document.querySelectorAll(".accordion__item-trigger").forEach((item) => {
+//   item.addEventListener("click", () => {
+//     const parent = item.parentNode;
+
+//     if (parent.classList.contains("accordion__item--active")) {
+//       parent.classList.remove("accordion__item--active");
+//     } else {
+//       document
+//       .querySelectorAll(".accordion__item")
+//       .forEach((item) => {
+//         item.classList.remove("accordion__item--active");
+//       });
+//     }
+//   });
+// });
